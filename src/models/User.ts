3b0ef0ccec82a5baf3744db7d1806224ac119352
Kenzie Bottoms <kenziebottoms@gmail.com>
2023-09-12 15:59:47 -0500

    sync with generics

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,5 +1,5 @@
-import axios, { AxiosResponse } from 'axios';
 import { Eventing } from './Eventing';
+import { Sync } from './Sync';
 
 export interface UserProps {
   id?: number;
@@ -9,6 +9,9 @@ export interface UserProps {
 
 export class User {
   public events: Eventing = new Eventing();
+  public sync: Sync<UserProps> = new Sync<UserProps>(
+    'http://localhost:3000/users'
+  );
 
   constructor(private data: UserProps) {}
 
