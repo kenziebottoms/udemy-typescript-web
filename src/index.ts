a0b111e1aea93f9e4e231d6d9bc00161afe974b3
Kenzie Bottoms <kenziebottoms@gmail.com>
2023-09-12 10:38:04 -0500

    eventing

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import { User } from './models/User';
 
-const user = new User({ id: 1 });
-user.set({ name: 'New Rip', age: 29 });
+const user = new User({ name: 'Goose' });
 user.save();
 
-const newUser = new User({ name: 'Maverick', age: 24 });
-newUser.save();
+user.events.on('change', () => {
+  console.log('change!');
+});
+
+user.events.trigger('change');
