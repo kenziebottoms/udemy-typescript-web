05d3d8e79cc622a059819f9f1be658319d66dfa2
Kenzie Bottoms <kenziebottoms@gmail.com>
2023-09-13 11:27:08 -0500

    arrow fns

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import { User } from './models/User';
 
 const user = new User({ name: 'Goose' });
-user.save();
 
-user.events.on('change', () => {
-  console.log('change!');
+console.log(user.get('name'));
+
+user.on('change', () => {
+  console.log('change');
 });
-
-user.events.trigger('change');
+user.trigger('change');
