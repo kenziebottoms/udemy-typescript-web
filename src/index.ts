0a172413d371c7d3a82aa52a8628be6c0e25a702
Kenzie Bottoms <kenziebottoms@gmail.com>
2023-09-25 14:14:44 -0500

    user list

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,20 @@
 import { UserEdit } from './views/UserEdit';
-import { User } from './models/User';
+import { User, UserProps } from './models/User';
+import { Collection } from './models/Collection';
+import { UserList } from './views/UserList';
 
-const user = User.buildUser({ name: 'Iceman', age: 28 });
-
-const root = document.getElementById('root');
-if (root) {
-  const userEdit = new UserEdit(root, user);
-  userEdit.render();
-  console.log(userEdit);
-} else {
-  throw new Error('Root element not found');
-}
+const users = new Collection(
+  'http://localhost:3000/users',
+  (json: UserProps) => {
+    return User.buildUser(json);
+  }
+);
+users.fetch();
+users.on('change', () => {
+  const root = document.getElementById('root');
+  if (root) {
+    new UserList(root, users).render();
+  } else {
+    throw new Error('Root element not found');
+  }
+});
